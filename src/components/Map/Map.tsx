8bbd8db1dd33fe5a236fc891e1bef806b2133b97
dLeczycki <danielleczycki@gmail.com>
2022-05-13 07:52:18 +0200

    Implement basic leaflet map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,26 @@
 
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import '../../utils/fix-map-icon';
+
+import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 export const Map = () => {
   
   return (
     <div className="Map">
-        ...
+        <MapContainer center={[51.9668421,20.1501398]} zoom={13}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+          />
+          <Marker position={[51.9668421,20.1501398]}>
+            <Popup>
+              <h2>Skierniewice Railway Station</h2>
+              <p>Place never to forget.. </p>
+            </Popup>
+          </Marker>
+        </MapContainer>
     </div>
   )
 }
